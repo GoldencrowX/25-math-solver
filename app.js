37aa766f002c0eb0982f6a5aebc128d8bd219667
GoldencrowX <rawipha65@gmail.com>
2026-02-12 10:45:56 +0700

    update

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello, This Is The Default!");
+});
+
 app.get('/math/factorial', (req, res) => {
-    let f = 1; for(let i=1; i<=req.query.n; i++) f*=i; res.json({ factorial: f });
+    let f = 1;
+    for(let i = 1; i <= req.query.n; i++) f *= i;
+    res.json({ factorial: f });
 });
 
 app.get('/math/fibonacci', (req, res) => {
@@ -11,10 +17,11 @@ app.get('/math/fibonacci', (req, res) => {
 });
 
 app.get('/math/prime', (req, res) => {
-    const n = req.query.n; res.json({ isPrime: n > 1 });
+    const n = req.query.n;
+    res.json({ isPrime: n > 1 });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
